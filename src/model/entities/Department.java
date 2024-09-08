9a9210f6b84bc7b0990e792a8f69bc8290ea383b
Will Costa <wcostarabr@hotmail.com>
2024-09-08 18:19:45 -0300

    Adding database access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,65 +1,57 @@
 package model.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Department implements Serializable {
-
+    
 	private static final long serialVersionUID = 1L;
-
+	
 	private Integer id;
-	private String name;
+    private String name;
 
-	public Department() {
-	}
+    public Department() {
+    }
 
-	public Department(Integer id, String name) {
-		this.id = id;
-		this.name = name;
-	}
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department that)) return false;
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Department other = (Department) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+        return Objects.equals(getId(), that.getId());
+    }
 
-	@Override
-	public String toString() {
-		return "Department [id=" + id + ", name=" + name + "]";
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Departament{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
