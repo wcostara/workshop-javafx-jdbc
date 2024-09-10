aafe32983cf90e6f9fde24c159eb62f20e6bf2f6
Will Costa <wcostarabr@hotmail.com>
2024-09-10 13:21:21 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,25 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 	
 	private Department depDepen;
+	
+	private DepartmentService depService;
 	
 	@FXML
 	private TextField txtId;
@@ -34,14 +42,42 @@ public class DepartmentFormController implements Initializable{
 		this.depDepen = depDepen;
 	}
 	
-	@FXML
-	public void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	public void setDepartmentService(DepartmentService depService) {
+		this.depService = depService;
 	}
 	
 	@FXML
-	public void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
+	public void onBtSaveAction(ActionEvent event) {
+		if(depDepen == null) {
+			throw new IllegalStateException("was null");
+		}
+		
+		if(depService == null) {
+			throw new IllegalStateException("service was null");
+		}
+		
+		try {		
+			depDepen = getFormData();
+			depService.saveOrUpdate(depDepen);
+			Utils.currentStage(event).close();
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+		}
+	}		
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getId()));
+		obj.setName(txtName.getText());
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 
 	@Override
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -14,6 +14,15 @@ public class DepartmentService {
 		return dao.findAll();
 	}
 	
+	public void saveOrUpdate(Department obj) {
+		if(obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
+	
 	
 
 }
